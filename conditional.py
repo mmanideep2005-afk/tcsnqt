f65f9e52ee449df94a044c3f1292de64735d6d19
Manideep <m.manideep2005@gmail.com>
2026-02-14 10:57:53 +0530

    conditional program

---
+++ conditional.py
@@ -0,0 +1,15 @@
+'''n ="10.35"
+print(int(float(n)))'''
+#check number is divisible by 2 or not 
+num = int(input("Enter a number :"))
+if(num%2==0):
+    print("it is divisible by 2")
+else:
+    print("it is not divisible by 2")
+a = 10
+b = 25
+if (a>b):
+    print("a is bigger")
+else:
+    print("b is bigger")
+print("program is done")    
