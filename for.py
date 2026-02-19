897f44a5b744258871595324bb8cdaf41f980025
Manideep <m.manideep2005@gmail.com>
2026-02-19 18:34:47 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,36 @@
+'''for i in range(1,11):
+    print(i)'''
+'''n=1
+while n<=10:
+    print(n)
+    n+=1'''
+'''sum=0
+for i in range(1,11):
+    sum+=i
+print(sum)'''
+'''n=1
+sum=0
+while(n<=10):
+    sum=sum+n
+    n+=1
+print(sum)'''
+string = str(input("Enter a string: "))
+n = len(string)
+count = 0
+count1 = 0                    
+for i in range(i>=n):
+    if string[i]== 'a' or 'e' or 'i' or 'o' or 'u':
+        print("Vowels found")
+        count = count+1
+    print(count)
+    i-=1
+    elif string[i] != 'a' or 'e' or 'i' or 'o' or 'u':
+        print("composite words found")
+        count1=count1+1
+    print(count1)
+        
+                   
+
+    
+
+
---
+++ loops.py
@@ -0,0 +1,45 @@
+'''num1,num2,num3 = map(int,input().split())
+if num1>=0:
+    if(num1%2==0):
+        print(f"{num1} is even")
+    else:
+        print(f"{num1} is odd")
+if num2>=0:
+    if(num2%2==0):
+        print(f"{num2} is even")
+    else:
+        print(f"{num2} is odd")        
+if num3>=0:
+    if(num3%2==0):
+        print(f"{num3} is even")
+    else:
+        print(f"{num3} is odd")'''  
+'''lst = list(map(int,input("enter values: ").split()))
+print(lst)
+for i in lst:
+    if i%2==0:
+        print(f"{i} is even")
+                     
+    else:
+        print(f"{i} is odd")'''
+'''for i in range(20,41,2):
+    print(i)'''
+'''for i in range(1,101):
+    print(i,end=" ")'''
+'''for i in range(10,41):
+    if i%2==0:
+        print(i,end=" ")'''
+'''for i in range(1,51):
+    if i%2==1:
+        print(i,end=" ")'''
+'''lst = [1,3,5,6]
+for i in range(0,4):
+    if lst[i]%2==0:
+        print(f"{lst[i]} is even number")
+    else:
+        print(f"{lst[i]} is odd number")'''      
+'''n = int(input("enter value :"))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print(sum)'''   
